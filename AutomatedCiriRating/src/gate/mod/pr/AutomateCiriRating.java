4e90dd4ee79ed24bfa34eb54ce322c0e262b8719
Josh <Josh@test.net>
2017-03-29 00:46:02 -0400

    init commit

---
+++ AutomatedCiriRating/src/gate/mod/pr/AutomateCiriRating.java
@@ -0,0 +1,36 @@
+/*
+ *  AutomateCiriRating.java
+ *
+ * Copyright (c) 2000-2012, The University of Sheffield.
+ *
+ * This file is part of GATE (see http://gate.ac.uk/), and is free
+ * software, licenced under the GNU Library General Public License,
+ * Version 3, 29 June 2007.
+ *
+ * A copy of this licence is included in the distribution in the file
+ * licence.html, and is also available at http://gate.ac.uk/gate/licence.html.
+ *
+ *  Josh, 29/3/2017
+ *
+ * For details on the configuration options, see the user guide:
+ * http://gate.ac.uk/cgi-bin/userguide/sec:creole-model:config
+ */
+
+package gate.mod.pr;
+
+import gate.*;
+import gate.creole.*;
+import gate.creole.metadata.*;
+import gate.util.*;
+
+
+/** 
+ * This class is the implementation of the resource AUTOMATEDCIRIRATING.
+ */
+@CreoleResource(name = "AutomatedCiriRating",
+        comment = "Add a descriptive comment about this resource")
+public class AutomateCiriRating  extends AbstractProcessingResource
+  implements ProcessingResource {
+
+
+} // class AutomateCiriRating
